576bcbe51f50a816a36d4fece8433440da08a7d5
mouadd55 <mouad_oudrib@outlook.com>
2023-06-27 14:06:08 -0400

    New update

--- main.h
+++ main.h
@@ -7,14 +7,14 @@
 # include <stdarg.h>
 # include <limits.h>
 
-int	_printf(const char *format, ...);
-void	_puthex(unsigned int nbr, char format, int *length);
-int	_putchar(char c);
+int     _putchar(char c);
 void	_putnbr(int nbr, int *length);
 void	_putstr(char *str, int *length);
+int     _printf(const char *format, ...);
 void	_putptr(unsigned long ptr, int *length);
-void	ft_puthex(unsigned long nbr, int *length);
 void	_putunnbr(unsigned int nbr, int *length);
+void	ft_puthex(unsigned long nbr, int *length);
 void	_format(va_list args, const char s, int *length);
+void	_puthex(unsigned int nbr, char format, int *length);
 
 #endif
