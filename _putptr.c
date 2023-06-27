334def65c5c7571fac406ae1516554850501639d
mouadd55 <mouad_oudrib@outlook.com>
2023-06-27 14:04:42 -0400

    Update %p

--- _putptr.c
+++ _putptr.c
@@ -37,7 +37,7 @@ void	ft_puthex(unsigned long nbr, int *length)
 void	_putptr(unsigned long ptr, int *length)
 {
 	if (!ptr)
-		*length += write(1, "(null)", 6);
+		*length += write(1, "(nil)", 5);
 	else
 	{
 		*length += write(1, "0x", 2);
